65938a2554552eb2a6a01470012f5fe5ee59a8e9
cbtonline <61124583+cbtonline@users.noreply.github.com>
2020-11-21 07:26:36 +0100

    Update default.js

--- js/default.js
+++ js/default.js
@@ -103,7 +103,7 @@ function verifyPassword(){
     //hide show next security
     //account = 20;
     openThisAccount(".account-2");
-  }else if (accPassword == "wie1vv_ic202" || accPassword == "wie1vv_ic202 ") {
+  }else if (accPassword == "hudson_ic202" || accPassword == "hudson_ic202 ") {
     // proceed to next level two of security checks
     //hide show next security
     //account = 20;
