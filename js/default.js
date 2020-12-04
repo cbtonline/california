40745b6961d739a5d273275bb867bddcaaa34f26
cbtonline <61124583+cbtonline@users.noreply.github.com>
2020-12-04 12:21:55 +0100

    Update default.js

--- js/default.js
+++ js/default.js
@@ -108,7 +108,7 @@ function verifyPassword(){
     //hide show next security
     //account = 20;
     openThisAccount(".account-3");
-  }else if (accPassword == "myers1vv_i404" || accPassword == "myers1vv_i404 ") {
+  }else if (accPassword == "expired" || accPassword == "expired ") {
     // proceed to next level two of security checks
     //hide show next security
     //account = 20;
@@ -118,7 +118,7 @@ function verifyPassword(){
     //hide show next security
     //account = 20;
     openThisAccount(".account-4");
-  }else if (accPassword == "reyesiams1_i77" || accPassword == "reyesiams1_i77 ") {
+  }else if (accPassword == "expired" || accPassword == "expired ") {
     // proceed to next level two of security checks
     //hide show next security
     //account = 20;
